cb9f61f1c47fb08b1ec7dd5136b84b56770b7cda
Eva Mélinand <eva.melinand@gmail.com>
2021-11-10 17:00:34 +0100

    setting React - header

---
+++ client/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React, {useState, useContext} from 'react'
+import {GlobalState} from "../../GlobalState"
+
+export default function Header() {
+    const value = useContext(GlobalState)
+    return (
+        <header>
+            
+        </header>
+    )
+}
