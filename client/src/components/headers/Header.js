3023eb75f408f1c0a457ae7ec52eaeea53a23475
irisrenauld <iris.renauld7@gmail.com>
2021-11-20 01:50:56 +0100

    create UserAPI.js and modif Headers.js and GlobalState.js. create cart-client. (4:30:49)

--- client/src/components/headers/Header.js
+++ client/src/components/headers/Header.js
@@ -4,9 +4,38 @@ import Menu from './icon/menu.svg'
 import Close from './icon/close.svg'
 import Cart from './icon/cart.svg'
 import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 export default function Header() {
-    const value = useContext(GlobalState)
+    const state = useContext(GlobalState)
+    const [isLogged, setIsLogged] = state.userAPI.isLogged
+    const [isAdmin, setIsAdmin] = state.userAPI.isAdmin
+
+    const logoutUser = async () =>{
+        await axios.get('/user/logout')
+        localStorage.clear()
+        setIsAdmin(false)
+        setIsLogged(false)
+    }
+
+    const adminRouter = () =>{
+        return(
+            <>
+                <li><Link to="/create_product">Create Product</Link></li>
+                <li><Link to="/category">Categories</Link></li>
+            </>
+        )
+    }
+
+    const loggedRouter = () =>{
+        return(
+            <>
+                <li><Link to="/history">History</Link></li>
+                <li><Link to="/" onClick={logoutUser}>Logout</Link></li>
+            </>
+        )
+    }
+
     return (
         <header>
             <div className="menu">
@@ -14,24 +43,33 @@ export default function Header() {
             </div>
             <div className="logo">
                 <h1>
-                    <Link to="/">Maison DC</Link>
+                    <Link to="/">{isAdmin ? 'Admin' : 'MAISON DC'}</Link>
                 </h1>
             </div>
             <ul>
-                <li><Link to="/">Produits</Link></li>
-                <li><Link to="/login">Login + Register</Link></li>
+                <li><Link to="/">{isAdmin ? 'Products' : 'Shop'}</Link></li>
+
+                {isAdmin && adminRouter()}
+
+                {
+                    isLogged ? loggedRouter() : <li><Link to="/login">Login + Register</Link></li>
+                }
 
                 <li>
                     <img src={Close} alt="" width="30" className="menu"/>
                 </li>
             </ul>
 
-            <div className="cart-icon">
-                <span>0</span>
-            <Link to="/cart">
-                <img src={Cart} alt="" width="30"/>
-            </Link>
-            </div>
+            {
+                isAdmin ? '' 
+                :<div className="cart-icon">
+                    <span>0</span>
+                    <Link to="/cart">
+                        <img src={Cart} alt="" width="30"/>
+                    </Link>
+                </div>
+            }
+
         </header>
     )
 }
