cbc34d6201ba9e0214a2a57c318bf1e981ffe9fa
Corentine <renuart.corentine@gmail.com>
2021-11-24 17:18:48 +0100

    Bloc 1 of the home page

--- client/src/components/mainpages/Homepage/DecorationDInterieur.js
+++ client/src/components/mainpages/Homepage/DecorationDInterieur.js
@@ -12,20 +12,28 @@ const DecorationDInterieur = () => {
 
         // Container
         <div className="DecorationDInterieur">
-            {/* Part 1 */}
-            <div className="DecorationDInterieur_left">
-                <h1 className="DecorationDInterieurh1">Décoration d'intérieur à Hélécine</h1>
-                <p><q>Si le soleil entre dans votre maison, il est un peu dans votre coeur</q> - Le Corbusier</p>
-                <button className="Homestaging">Home staging</button>
-            </div>
-            {/* Part 2 */}
-            <div className="DecorationDInterieur_right">
-                {/* Images */}
-                <img src={Cadre} className="squareimg" alt="Image de cadre"/>
-                <img src={Logo} className="logoimg" alt="Logo de la maison DC"/>
-                <img src={Salon} className="salonimg" alt="Image de salon décoré"/>
-                <img src={Lumiere} className="squareimg" alt="Image de lampe"/>
-            </div>
+
+
+
+                {/* Part 1 */}
+                <div className="DecorationDInterieur_left">
+                    <h1 className="DecorationDInterieurh1">Décoration d'intérieur à Hélécine</h1>
+                    <p><q>Si le soleil entre dans votre maison, il est un peu dans votre coeur</q> - Le Corbusier</p>
+                    <button className="Homestaging">Home staging</button>
+                </div>
+                {/* Part 2 */}
+                <div className="DecorationDInterieur_right">
+                    {/* Images */}
+                    <img src={Cadre} className="squareimg" alt="Image de cadre"/>
+                    <img src={Logo} className="logoimg" alt="Logo de la maison DC"/>
+                    <img src={Salon} className="salonimg" alt="Image de salon décoré"/>
+                    <img src={Lumiere} className="squareimg" alt="Image de lampe"/>
+                </div>
+
+
+
+
+
         </div>
     );
 };
