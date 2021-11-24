840664abe2a310aba0c2b1570967b70faaf20577
Eva Mélinand <eva.melinand@gmail.com>
2021-11-24 10:06:31 +0100

    login + register / bug with the login part(infinte loading) to fix

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -9,7 +9,7 @@ export const DataProvider = ({children}) => {
 
 
     const refreshToken = async () =>{
-        const token = await axios.get('/user/refresh_token')
+        const res = await axios.get('/user/refresh_token')
         setToken(res.data.accesstoken)
     }
 
