6b19c491c6937c20acb9e2d7d154629d67a866cc
irisrenauld <iris.renauld7@gmail.com>
2022-01-12 11:46:09 +0100

    last change for deployment

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
+const path = require('path')
 
 
 const app = express()
@@ -47,6 +48,12 @@ app.get('/', (req, res) =>{
     res.json({msg:'Welcome'})
 })
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req,res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5500
 app.listen(PORT, () => {
